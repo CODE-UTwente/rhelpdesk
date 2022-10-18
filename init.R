699d540e85b77a892d0b838fe6a5bde169220cc8
Wouter Stoter <w.d.stoter@student.utwente.nl>
2022-10-18 12:12:03 +0200

    Re-knitted website with new changes

--- init.R
+++ init.R
@@ -1,3 +1,3 @@
 install.packages('formatR', repos = 'http://cran.rstudio.com', dependencies = TRUE)
-rmarkdown::render('book/Intro R for RM&DS.Rmd', "pdf_document")
+rmarkdown::render('book/Intro R for RM_DS.Rmd', "pdf_document")
 rmarkdown::render_site(encoding = 'UTF-8')
